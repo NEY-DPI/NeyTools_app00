6e6c72f82132234d79827e9ee0190c325d212f71
Denis Piron <dpi@ney.partners>
2023-12-01 16:16:06 +0100

    Initial commit

---
+++ streamlit_app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.write("Hello!")
+
+st.write("This is the homepage of NeyTools")
+st.write("This page is public and accessible everywhere, but the links to the tools below are only accessible within Ney server")
+
+st.write("List of tools, with date and status of upload etc :")
+
+## app01
+st.write("Tool 1 link : http://10.0.10.14:8501/")
+
+## app02
+st.write("Tool 2 link : http://10.0.10.14:8502/")
+
